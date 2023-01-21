7246f0e18ffde480d214228b1c5a8ad278b73b8f
Cordu7 <calendulagunst@hotmail.com>
2023-01-21 15:43:51 +0000

    passsenger tests

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger1;
+
+    @Before
+    public void before(){
+        passenger1 = new Passenger("Cordu", 2);
+    }
+
+    @Test
+    public void passengerHasName(){
+        assertEquals("Cordu", passenger1.getName());
+    }
+    @Test
+    public void passengerHasBags(){
+        assertEquals(2, passenger1.getBags());
+    }
 }
